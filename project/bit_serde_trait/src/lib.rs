f1c4fa23a560b0c49b029aec683493d08f5be3bf
M Maxim <mmaxim2291@gmail.com>
2023-12-15 10:42:49 +0200

    implementation for vectors

--- project/bit_serde_trait/src/lib.rs
+++ project/bit_serde_trait/src/lib.rs
@@ -5,30 +5,85 @@ pub use ::bit_serde_macro::*;
 extern crate byteorder;
 use bitvec::prelude::*;
 use bitvec::view::BitView;
-use byteorder::{BigEndian, ByteOrder, LittleEndian};
+
+//           Main traits
 
 pub trait BitSerdeDeserialization {
+    const SIZE_IN_BITS: usize;
     fn deserialize(data: &Vec<u8>) -> Self;
-    fn deserialize_from(data: &BitVec<u8, Lsb0>) -> Self;
+    fn deserialize_from(data: &BitSlice<u8, Lsb0>) -> (&BitSlice<u8, Lsb0>, Self);
 }
 pub trait BitSerdeSerialization {
     fn serialize(&self) -> std::io::Result<Vec<u8>>;
     fn write_bits_to(&self, destination: &mut BitVec<u8, Lsb0>) -> std::io::Result<()>;
 }
 
+//      IMplementation for Vectors
 impl<E: BitSerdeSerialization> BitSerdeSerialization for Vec<E> {
     fn serialize(&self) -> std::io::Result<Vec<u8>> {
-        let data = Vec::<u8>::new();
-        let next = 0usize;
+        let mut destination = bitvec!(u8,Lsb0;);
 
-        //self.write_starting_from(&data, next)?;
+        self.write_bits_to(&mut destination)?;
 
-        Ok(data)
+        Ok(destination.into())
     }
     fn write_bits_to(&self, destination: &mut BitVec<u8, Lsb0>) -> std::io::Result<()> {
+        let len = self.len();
+
+        destination.extend(len.view_bits::<Lsb0>());
+
+        for el in self.iter() {
+            el.write_bits_to(destination)?;
+        }
+
         Ok(())
     }
 }
+
+impl<E: BitSerdeDeserialization> BitSerdeDeserialization for Vec<E> {
+    const SIZE_IN_BITS: usize = <E>::SIZE_IN_BITS;
+
+    fn deserialize(data: &Vec<u8>) -> Self {
+        let bs = data.view_bits::<Lsb0>();
+
+        BitSerdeDeserialization::deserialize_from(bs).1
+    }
+    fn deserialize_from(mut data: &BitSlice<u8, Lsb0>) -> (&bitvec::slice::BitSlice<u8>, Vec<E>) {
+        const SIZE: usize = std::mem::size_of::<usize>();
+
+        let bytes = data[0..SIZE * 8].to_bitvec().into_vec();
+
+        if bytes.len() != SIZE {
+            panic!("uneuqal sizes");
+        }
+
+        let mut x: [u8; SIZE] = [0; SIZE];
+
+        let mut it = 0u8;
+        for i in bytes {
+            x[it as usize] = i;
+            it += 1;
+        }
+
+        let len: usize = <usize>::from_le_bytes(x);
+        println!("size of vec = {len}");
+
+        let mut vec = Vec::<E>::with_capacity(len);
+
+        data = &data[SIZE * 8..];
+
+        for _ in 0..len {
+            let result = BitSerdeDeserialization::deserialize_from(data);
+
+            vec.push(result.1);
+            data = result.0;
+        }
+
+        (data, vec)
+    }
+}
+
+// Implementation for number types
 use std::mem;
 macro_rules! implement_traits_for_unsigned_types {
     ($($t:ty),*) => {
@@ -44,36 +99,25 @@ macro_rules! implement_traits_for_unsigned_types {
                 }
                 fn write_bits_to(&self, destination: &mut BitVec<u8,Lsb0>) -> std::io::Result<()> {
 
-                    let bytes = self.to_ne_bytes();
+                    let bytes = self.to_le_bytes();
 
-                    destination.extend(bytes.iter()); 
+                    destination.extend(bytes.iter());
 
                     Ok(())
                 }
             }
 
             impl BitSerdeDeserialization for $t {
+                const SIZE_IN_BITS:usize = mem::size_of::<$t>();
 
                 fn deserialize(data: &Vec<u8>) -> Self {
 
-                    const SIZE : usize = mem::size_of::<$t>();
+                    let bs = data.view_bits::<Lsb0>();
 
-                    if data.len() != SIZE {
-                        panic!("unequal sizes");
-                    }
-
-                    let mut x: [u8;SIZE] = [0;SIZE];
-
-                    let mut it = 0u8;
-
-                    for i in data {
-                        x[it as usize] = *i;
-                        it+=1;
-                    }
-
-                    <$t>::from_ne_bytes(x)
+                    BitSerdeDeserialization::deserialize_from(bs).1
                 }
-                fn deserialize_from(data: &BitVec<u8,Lsb0>) -> Self {
+
+                fn deserialize_from(mut data: &BitSlice<u8,Lsb0>) -> (&BitSlice<u8, Lsb0>,Self) {
 
                     const SIZE : usize = mem::size_of::<$t>();
 
@@ -86,8 +130,9 @@ macro_rules! implement_traits_for_unsigned_types {
                         x[it as usize] = i;
                         it+=1;
                     }
+                    data = &data[SIZE*8..];
 
-                    <$t>::from_ne_bytes(x)
+                    (data,<$t>::from_le_bytes(x))
                 }
             }
         )*
--- project/src/main.rs
+++ project/src/main.rs
@@ -1,10 +1,7 @@
-use std::collections::btree_map::VacantEntry;
-
-use std::str::from_boxed_utf8_unchecked;
-
 use bit_serde_trait::bit_serde;
 use bit_serde_trait::BitSerdeDeserialization;
 use bit_serde_trait::BitSerdeSerialization;
+use bitvec::bitvec;
 
 /*#[bit_serde(Deserialize,Serialize)]
 struct myStr {
@@ -13,23 +10,20 @@ struct myStr {
     name: String
 }
  */
-extern crate byteorder;
-use bitvec::view::BitView;
-use byteorder::ReadBytesExt;
-use byteorder::{BigEndian, ByteOrder, LittleEndian};
 
 use bitvec::prelude::*;
-
-use std::mem;
 fn main() -> std::io::Result<()> {
-    let num = 32.5f64;
-    let bytes: Vec<u8> = num.serialize()?;
-    //let num =  <f32>::deserialize(&bytes);
+    let vec = vec![13u8, 15, 17, 19];
 
-    let bytes: BitVec<u8, Lsb0> = BitVec::from_vec(bytes);
+    let mut bv = bitvec!(u8,Lsb0;);
+    
+    vec.write_bits_to(&mut bv)?;
 
-    let num = <f64>::deserialize_from(&bytes);
+    let vec = bv.into_vec();
+    let bs = vec.view_bits::<Lsb0>();
 
-    println!("num = {num}");
+    let vec : Vec<u8> = BitSerdeDeserialization::deserialize_from(&bs).1;
+
+    println!("{:?}",vec);
     Ok(())
 }
